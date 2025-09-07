faffa141e35782f4c0f65ecb42effbad6bf3339f
Michael526-dev <michael.chia@bunkerchain.io>
2025-09-07 16:58:06 +0800

    first commit

---
+++ src/main/java/com/michael/springboot/thymeleafdemo/Controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.michael.springboot.thymeleafdemo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(Model myModel){
+        myModel.addAttribute("theDate", new java.util.Date());
+        return "helloworld";
+    }
+}
