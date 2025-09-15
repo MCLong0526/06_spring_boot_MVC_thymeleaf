69cc340eca9713da4d0abf726d0a3575fdda24eb
Michael526-dev <michael.chia@bunkerchain.io>
2025-09-16 07:26:33 +0800

    learn dropdown, and radio selection in form

--- src/main/java/com/michael/springboot/thymeleafdemo/Controller/StudentController.java
+++ src/main/java/com/michael/springboot/thymeleafdemo/Controller/StudentController.java
@@ -1,19 +1,35 @@
 package com.michael.springboot.thymeleafdemo.Controller;
 
 import com.michael.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${colors}")
+    private List<String> colors;
+
+    @Value("${programmingLanguages}")
+    private List<String> programmingLanguages;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
         Student theStudent = new Student();
         theModel.addAttribute("student", theStudent);
+        theModel.addAttribute("countries", countries);
+        theModel.addAttribute("colors", colors);
+        theModel.addAttribute("programmingLanguages", programmingLanguages);
         return "student-form";
     }
 
--- src/main/java/com/michael/springboot/thymeleafdemo/model/Student.java
+++ src/main/java/com/michael/springboot/thymeleafdemo/model/Student.java
@@ -3,8 +3,12 @@ package com.michael.springboot.thymeleafdemo.model;
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
+    private String color;
+    private String programmingLanguage;
 
-    public Student(){}
+    public Student() {
+    }
 
     public String getFirstName() {
         return firstName;
@@ -21,4 +25,29 @@ public class Student {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public void setCountry(String country) {
+        this.country =country;
+    }
+
+    public String getCountry(){
+        return country;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage){
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getProgrammingLanguage(){
+        return programmingLanguage;
+    }
+
 }
